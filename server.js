5664a94ebc94e5d07af33f50faa5bb60b58bf590
jeanhern81 <jeanhern81@gmail.com>
2020-08-03 14:22:01 -0700

    got local server working

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-var express = require("express");
-
-var PORT = process.env.PORT || 8080;
+var express = require('express');
+var exphbs  = require('express-handlebars');
+var connection = require("./config/connection");
+var bodyParser = require('body-parser')
 
 var app = express();
+
+var PORT = process.env.PORT || 8080;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
 // Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+//app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+//app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burger_controller.js");
+//var routes = require("./controllers/burger_controllers");
 
-app.use(routes);
+//app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+console.log("Server listening on: http://localhost:" + PORT);
 });
