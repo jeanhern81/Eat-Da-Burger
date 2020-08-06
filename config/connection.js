33d1903cd7f5ed907629d6650305439e823a799e
jeanhern81 <jeanhern81@gmail.com>
2020-08-06 13:45:55 -0700

    trying to fix deployment problem

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,15 @@ var mysql = require("mysql");
 
 var connection;
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL); 
+
 } else {
     connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "Mercury35!",
-    database: "burger_db",
+    database: "burger_db"
     });
 };
 
